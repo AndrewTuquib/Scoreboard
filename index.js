fda72b187eda6a282c451012a6f1aa210ab309a4
AndrewTuquib <andrew@jov-t.com>
2022-08-09 21:06:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let homeScore = document.getElementById('home-score')
-let guestScore = document.getElementById('guest-score')
+const homeScore = document.getElementById('home-score')
+const guestScore = document.getElementById('guest-score')
 let currentHomeScore = 0
 let currentGuestScore = 0
-let add1Home = document.getElementById('add1home-btn')
-let add2Home = document.getElementById('add2home-btn')
-let add3Home = document.getElementById('add3home-btn')
-let add1Guest = document.getElementById('add1guest-btn')
-let add2Guest = document.getElementById('add2guest-btn')
-let add3Guest = document.getElementById('add3guest-btn')
-let reset = document.getElementById('reset-btn')
+const add1Home = document.getElementById('add1home-btn')
+const add2Home = document.getElementById('add2home-btn')
+const add3Home = document.getElementById('add3home-btn')
+const add1Guest = document.getElementById('add1guest-btn')
+const add2Guest = document.getElementById('add2guest-btn')
+const add3Guest = document.getElementById('add3guest-btn')
+const reset = document.getElementById('reset-btn')
 
 
 function leader() {
@@ -24,52 +24,51 @@ function leader() {
     }
 }
 
-function updateGuestScore() {
-    guestScore.textContent = currentGuestScore
-}
-function updateHomeScore() {
-    homeScore.textContent = currentHomeScore
+
+
+function updateScore(score, currentScore) {
+    score.textContent = currentScore
+    leader()
 }
 
+function addHomeScore(increment){
+    currentHomeScore += increment
+}
+function addGuestScore(increment){
+    currentGuestScore += increment
+}
 
 add1Home.addEventListener('click', function() {
-    currentHomeScore += 1
-    updateHomeScore()
-    leader()
+    addHomeScore(1)
+    updateScore(homeScore, currentHomeScore)
 })
 add2Home.addEventListener('click', function() {
-    currentHomeScore += 2
-    updateHomeScore()
-    leader()
+    addHomeScore(2)
+    updateScore(homeScore, currentHomeScore)
 })
 add3Home.addEventListener('click', function() {
-    currentHomeScore += 3
-    updateHomeScore()
-    leader()
+    addHomeScore(3)
+    updateScore(homeScore, currentHomeScore)
 })
 
 add1Guest.addEventListener('click', function(){
-    currentGuestScore += 1
-    updateGuestScore()
-    leader()
+    addGuestScore(1)
+    updateScore(guestScore, currentGuestScore)
 })
 add2Guest.addEventListener('click', function(){
-    currentGuestScore += 2
-    updateGuestScore()
-    leader()
+    addGuestScore(2)
+    updateScore(guestScore, currentGuestScore)
 })
 add3Guest.addEventListener('click', function(){
-    currentGuestScore += 3
-    updateGuestScore()
-    leader()
+    addGuestScore(3)
+    updateScore(guestScore, currentGuestScore)
 })
 
 reset.addEventListener('click', function(){
     currentGuestScore *= 0
-    updateGuestScore()
     currentHomeScore *= 0
-    updateHomeScore()
-    leader()
+    updateScore(guestScore, currentGuestScore)
+    updateScore(homeScore, currentHomeScore)
 })
 
 
